0aaa8534fc0a3737d46bf21e7591a077f62c5855
Muhammad Akram Khan <muhammadakramkhan.dev@gmail.com>
2025-10-22 23:35:29 +0500

    added dummy UI for reset

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager Instance;
+
+    [Header("UI Elements")]
+    [SerializeField] private GameObject resetPanel;
+
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        if (resetPanel != null)
+            resetPanel.SetActive(false);
+    }
+
+    public void ShowResetPanelDelayed(float delay)
+    {
+        Invoke(nameof(ShowResetPanel), delay);
+    }
+
+    public void ShowResetPanel()
+    {
+        if (resetPanel != null)
+            resetPanel.SetActive(true);
+    }
+
+    public void OnResetButtonPressed()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
