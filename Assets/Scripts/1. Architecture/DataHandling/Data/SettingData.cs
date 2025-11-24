e3a30d5b6ec812ee75ff98ac8421a1ba2aac868d
Muhammad Akram Khan <muhammadakramkhan.dev@gmail.com>
2025-11-24 12:04:57 +0500

    added music/sound settings

--- Assets/Scripts/1. Architecture/DataHandling/Data/SettingData.cs
+++ Assets/Scripts/1. Architecture/DataHandling/Data/SettingData.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine;
 
 [System.Serializable]
 public class SettingData
@@ -7,14 +6,19 @@ public class SettingData
     public bool musicSetting;
     public bool soundSetting;
     public bool vibrationSetting;
+    public float musicVolume;
+    public float effectsVolume;
 
     public SettingData()
     {
         musicSetting = true;
         soundSetting = true;
         vibrationSetting = true;
+        musicVolume = 0.5f;
+        effectsVolume = 0.7f;
     }
 
+    // Music Toggle
     public void MusicSettingChange(bool value)
     {
         musicSetting = value;
@@ -25,6 +29,7 @@ public class SettingData
         return musicSetting;
     }
 
+    // Sound Toggle
     public void SoundSettingChange(bool value)
     {
         soundSetting = value;
@@ -35,6 +40,7 @@ public class SettingData
         return soundSetting;
     }
 
+    // Vibration Toggle
     public void VibrationSettingChange(bool value)
     {
         vibrationSetting = value;
@@ -44,4 +50,64 @@ public class SettingData
     {
         return vibrationSetting;
     }
+
+    // Volume Controls
+    public void SetMusicVolume(float volume)
+    {
+        musicVolume = Mathf.Clamp01(volume);
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        effectsVolume = Mathf.Clamp01(volume);
+    }
 }
+
+//************* original working code ******************
+// using System.Collections;
+// using System.Collections.Generic;
+
+// [System.Serializable]
+// public class SettingData
+// {
+//     public bool musicSetting;
+//     public bool soundSetting;
+//     public bool vibrationSetting;
+
+//     public SettingData()
+//     {
+//         musicSetting = true;
+//         soundSetting = true;
+//         vibrationSetting = true;
+//     }
+
+//     public void MusicSettingChange(bool value)
+//     {
+//         musicSetting = value;
+//     }
+
+//     public bool IsMusicSettingON()
+//     {
+//         return musicSetting;
+//     }
+
+//     public void SoundSettingChange(bool value)
+//     {
+//         soundSetting = value;
+//     }
+
+//     public bool IsSoundSettingON()
+//     {
+//         return soundSetting;
+//     }
+
+//     public void VibrationSettingChange(bool value)
+//     {
+//         vibrationSetting = value;
+//     }
+
+//     public bool IsVibrationSettingON()
+//     {
+//         return vibrationSetting;
+//     }
+// }
--- Assets/Scripts/1. Architecture/Managers/SettingsManager.cs
+++ Assets/Scripts/1. Architecture/Managers/SettingsManager.cs
@@ -1,3 +1,6 @@
+// ========================================
+// 1. SettingsManager.cs - UPDATED TO WORK WITH YOUR AUDIOMANAGER
+// ========================================
 using System;
 using UnityEngine;
 using TMPro;
@@ -16,44 +19,81 @@ public class SettingsManager : MonoBehaviour
         public string alertOffString;
     }
 
-    [SerializeField] DataManager dataManager;
-    [SerializeField] VibrationController vibrationController;
+    [SerializeField] private DataManager dataManager;
+    [SerializeField] private VibrationController vibrationController;
 
-    [SerializeField] SettingObject music;
-    [SerializeField] SettingObject sound;
-    [SerializeField] SettingObject vibration;
+    [Header("Setting Toggles")]
+    [SerializeField] private SettingObject music;
+    [SerializeField] private SettingObject sound;
+    [SerializeField] private SettingObject vibration;
+
+    [Header("Volume Sliders (Optional)")]
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider effectsVolumeSlider;
+
     [Header("Alert Animation")]
     [SerializeField] private TextMeshProUGUI alertText;
     [SerializeField] private Transform alertTargetPos;
     [SerializeField] private CanvasGroup alertTextCanvasGroup;
+
     private Vector3 alertStartPos;
-    private int musicValue, soundValue;
     private Tween alertTween;
 
     private void Start()
     {
-        alertText.gameObject.SetActive(false);
-        alertStartPos = alertText.transform.position;
+        if (alertText != null)
+        {
+            alertText.gameObject.SetActive(false);
+            alertStartPos = alertText.transform.position;
+        }
+        
         InitializeSettings();
+        InitializeSliders();
     }
+
     private void InitializeSettings()
     {
-        SetSetting(music, dataManager.gameData.setting.IsMusicSettingON());
-        SoundsManager.instance.MusicSetting(dataManager.gameData.setting.IsMusicSettingON());
-     
-        SetSetting(sound, dataManager.gameData.setting.IsSoundSettingON());
-        SoundsManager.instance.SoundSetting(dataManager.gameData.setting.IsSoundSettingON());
+        // Music Setting
+        bool isMusicOn = dataManager.gameData.setting.IsMusicSettingON();
+        SetSettingVisual(music, isMusicOn);
+        ApplyMusicSetting(isMusicOn, false); // false = don't show alert on init
 
-        SetSetting(vibration, dataManager.gameData.setting.IsVibrationSettingON());
-        vibrationController.VibrationSetting(dataManager.gameData.setting.IsVibrationSettingON());
+        // Sound Setting
+        bool isSoundOn = dataManager.gameData.setting.IsSoundSettingON();
+        SetSettingVisual(sound, isSoundOn);
+        ApplySoundSetting(isSoundOn, false);
+
+        // Vibration Setting
+        bool isVibrationOn = dataManager.gameData.setting.IsVibrationSettingON();
+        SetSettingVisual(vibration, isVibrationOn);
+        if (vibrationController != null)
+            vibrationController.VibrationSetting(isVibrationOn);
     }
 
-    private void SetSetting(SettingObject setting, bool value)
+    private void InitializeSliders()
+    {
+        // Setup Music Volume Slider
+        if (musicVolumeSlider != null)
+        {
+            float savedMusicVolume = dataManager.gameData.setting.musicVolume;
+            musicVolumeSlider.value = savedMusicVolume;
+            musicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
+        }
+
+        // Setup Effects Volume Slider
+        if (effectsVolumeSlider != null)
+        {
+            float savedEffectsVolume = dataManager.gameData.setting.effectsVolume;
+            effectsVolumeSlider.value = savedEffectsVolume;
+            effectsVolumeSlider.onValueChanged.AddListener(OnEffectsVolumeChanged);
+        }
+    }
+
+    private void SetSettingVisual(SettingObject setting, bool value)
     {
         SetImageSprite(setting.image, value ? setting.onSprite : setting.offSprite);
-        alertText.text = value ? setting.alertOnString : setting.alertOffString;
-        PlayAlertAnimation();
     }
+
     private void SetImageSprite(Image image, Sprite sprite)
     {
         if (image != null && sprite != null)
@@ -61,42 +101,261 @@ public class SettingsManager : MonoBehaviour
             image.sprite = sprite;
         }
     }
-    private void PlayAlertAnimation()
+
+    private void PlayAlertAnimation(string message)
     {
+        if (alertText == null) return;
+
         alertText.gameObject.SetActive(false);
         alertTween?.Kill();
         alertTextCanvasGroup.alpha = 1;
+        alertText.text = message;
         alertText.gameObject.SetActive(true);
 
-        float duration = 1;
+        float duration = 1f;
         alertText.transform.position = alertStartPos;
 
-        alertTween = alertText.transform.DOMove(alertTargetPos.position, duration).SetUpdate(true).OnComplete(() =>
-        {
-            alertTextCanvasGroup.DOFade(0, duration * 0.3f).SetUpdate(true).OnComplete(() =>
+        alertTween = alertText.transform.DOMove(alertTargetPos.position, duration)
+            .SetUpdate(true)
+            .OnComplete(() =>
             {
-                alertText.gameObject.SetActive(false);
+                alertTextCanvasGroup.DOFade(0, duration * 0.3f)
+                    .SetUpdate(true)
+                    .OnComplete(() =>
+                    {
+                        alertText.gameObject.SetActive(false);
+                    });
             });
-        });
     }
+
+    // ========== PUBLIC METHODS FOR UI BUTTONS ==========
+
     public void MusicToggle()
     {
-        dataManager.gameData.setting.MusicSettingChange(!dataManager.gameData.setting.IsMusicSettingON());
-        SetSetting(music, dataManager.gameData.setting.IsMusicSettingON());
-        SoundsManager.instance.MusicSetting(dataManager.gameData.setting.IsMusicSettingON());
+        bool newValue = !dataManager.gameData.setting.IsMusicSettingON();
+        dataManager.gameData.setting.MusicSettingChange(newValue);
+        SetSettingVisual(music, newValue);
+        ApplyMusicSetting(newValue, true);
+        
+        // Save the data
+        dataManager.SaveGameData();
+        
+        // Play button click sound
+        AudioManager.Instance?.PlayUI("ButtonClick");
     }
 
     public void SoundToggle()
     {
-        dataManager.gameData.setting.SoundSettingChange(!dataManager.gameData.setting.IsSoundSettingON());
-        SetSetting(sound, dataManager.gameData.setting.IsSoundSettingON());
-        SoundsManager.instance.SoundSetting(dataManager.gameData.setting.IsSoundSettingON());
+        bool newValue = !dataManager.gameData.setting.IsSoundSettingON();
+        dataManager.gameData.setting.SoundSettingChange(newValue);
+        SetSettingVisual(sound, newValue);
+        ApplySoundSetting(newValue, true);
+        
+        // Save the data
+        dataManager.SaveGameData();
+        
+        // Play button click sound
+        AudioManager.Instance?.PlayUI("ButtonClick");
     }
 
     public void VibrationToggle()
     {
-        dataManager.gameData.setting.VibrationSettingChange(!dataManager.gameData.setting.IsVibrationSettingON());
-        SetSetting(vibration, dataManager.gameData.setting.IsVibrationSettingON());
-        vibrationController.VibrationSetting(dataManager.gameData.setting.IsVibrationSettingON());
+        bool newValue = !dataManager.gameData.setting.IsVibrationSettingON();
+        dataManager.gameData.setting.VibrationSettingChange(newValue);
+        SetSettingVisual(vibration, newValue);
+        
+        if (vibrationController != null)
+            vibrationController.VibrationSetting(newValue);
+        
+        string message = newValue ? vibration.alertOnString : vibration.alertOffString;
+        PlayAlertAnimation(message);
+        
+        // Save the data
+        dataManager.SaveGameData();
+        
+        // Play button click sound
+        AudioManager.Instance?.PlayUI("ButtonClick");
+    }
+
+    public void OnMusicVolumeChanged(float volume)
+    {
+        dataManager.gameData.setting.SetMusicVolume(volume);
+        
+        if (dataManager.gameData.setting.IsMusicSettingON())
+        {
+            AudioManager.Instance?.SetMusicVolume(volume);
+        }
+        
+        // Save the data
+        dataManager.SaveGameData();
+    }
+
+    public void OnEffectsVolumeChanged(float volume)
+    {
+        dataManager.gameData.setting.SetEffectsVolume(volume);
+        
+        if (dataManager.gameData.setting.IsSoundSettingON())
+        {
+            AudioManager.Instance?.SetSFXVolume(volume);
+            AudioManager.Instance?.SetUIVolume(volume);
+        }
+        
+        // Save the data
+        dataManager.SaveGameData();
+    }
+
+    // ========== PRIVATE HELPER METHODS ==========
+
+    private void ApplyMusicSetting(bool isOn, bool showAlert)
+    {
+        if (AudioManager.Instance == null) return;
+
+        if (isOn)
+        {
+            // Turn music ON with saved volume
+            float savedVolume = dataManager.gameData.setting.musicVolume;
+            AudioManager.Instance.SetMusicVolume(savedVolume);
+            
+            if (showAlert)
+                PlayAlertAnimation(music.alertOnString);
+        }
+        else
+        {
+            // Turn music OFF (mute)
+            AudioManager.Instance.SetMusicVolume(0f);
+            
+            if (showAlert)
+                PlayAlertAnimation(music.alertOffString);
+        }
+    }
+
+    private void ApplySoundSetting(bool isOn, bool showAlert)
+    {
+        if (AudioManager.Instance == null) return;
+
+        if (isOn)
+        {
+            // Turn sound ON with saved volume
+            float savedVolume = dataManager.gameData.setting.effectsVolume;
+            AudioManager.Instance.SetSFXVolume(savedVolume);
+            AudioManager.Instance.SetUIVolume(savedVolume);
+            
+            if (showAlert)
+                PlayAlertAnimation(sound.alertOnString);
+        }
+        else
+        {
+            // Turn sound OFF (mute)
+            AudioManager.Instance.SetSFXVolume(0f);
+            AudioManager.Instance.SetUIVolume(0f);
+            
+            if (showAlert)
+                PlayAlertAnimation(sound.alertOffString);
+        }
     }
 }
+
+
+// *********** original setting manager script ******************
+// using System;
+// using UnityEngine;
+// using TMPro;
+// using UnityEngine.UI;
+// using DG.Tweening;
+
+// public class SettingsManager : MonoBehaviour
+// {
+//     [Serializable]
+//     public struct SettingObject
+//     {
+//         public Sprite onSprite;
+//         public Sprite offSprite;
+//         public Image image;
+//         public string alertOnString;
+//         public string alertOffString;
+//     }
+
+//     [SerializeField] DataManager dataManager;
+//     [SerializeField] VibrationController vibrationController;
+
+//     [SerializeField] SettingObject music;
+//     [SerializeField] SettingObject sound;
+//     [SerializeField] SettingObject vibration;
+//     [Header("Alert Animation")]
+//     [SerializeField] private TextMeshProUGUI alertText;
+//     [SerializeField] private Transform alertTargetPos;
+//     [SerializeField] private CanvasGroup alertTextCanvasGroup;
+//     private Vector3 alertStartPos;
+//     private int musicValue, soundValue;
+//     private Tween alertTween;
+
+//     private void Start()
+//     {
+//         alertText.gameObject.SetActive(false);
+//         alertStartPos = alertText.transform.position;
+//         InitializeSettings();
+//     }
+//     private void InitializeSettings()
+//     {
+//         SetSetting(music, dataManager.gameData.setting.IsMusicSettingON());
+//         SoundsManager.instance.MusicSetting(dataManager.gameData.setting.IsMusicSettingON());
+     
+//         SetSetting(sound, dataManager.gameData.setting.IsSoundSettingON());
+//         SoundsManager.instance.SoundSetting(dataManager.gameData.setting.IsSoundSettingON());
+
+//         SetSetting(vibration, dataManager.gameData.setting.IsVibrationSettingON());
+//         vibrationController.VibrationSetting(dataManager.gameData.setting.IsVibrationSettingON());
+//     }
+
+//     private void SetSetting(SettingObject setting, bool value)
+//     {
+//         SetImageSprite(setting.image, value ? setting.onSprite : setting.offSprite);
+//         alertText.text = value ? setting.alertOnString : setting.alertOffString;
+//         PlayAlertAnimation();
+//     }
+//     private void SetImageSprite(Image image, Sprite sprite)
+//     {
+//         if (image != null && sprite != null)
+//         {
+//             image.sprite = sprite;
+//         }
+//     }
+//     private void PlayAlertAnimation()
+//     {
+//         alertText.gameObject.SetActive(false);
+//         alertTween?.Kill();
+//         alertTextCanvasGroup.alpha = 1;
+//         alertText.gameObject.SetActive(true);
+
+//         float duration = 1;
+//         alertText.transform.position = alertStartPos;
+
+//         alertTween = alertText.transform.DOMove(alertTargetPos.position, duration).SetUpdate(true).OnComplete(() =>
+//         {
+//             alertTextCanvasGroup.DOFade(0, duration * 0.3f).SetUpdate(true).OnComplete(() =>
+//             {
+//                 alertText.gameObject.SetActive(false);
+//             });
+//         });
+//     }
+//     public void MusicToggle()
+//     {
+//         dataManager.gameData.setting.MusicSettingChange(!dataManager.gameData.setting.IsMusicSettingON());
+//         SetSetting(music, dataManager.gameData.setting.IsMusicSettingON());
+//         SoundsManager.instance.MusicSetting(dataManager.gameData.setting.IsMusicSettingON());
+//     }
+
+//     public void SoundToggle()
+//     {
+//         dataManager.gameData.setting.SoundSettingChange(!dataManager.gameData.setting.IsSoundSettingON());
+//         SetSetting(sound, dataManager.gameData.setting.IsSoundSettingON());
+//         SoundsManager.instance.SoundSetting(dataManager.gameData.setting.IsSoundSettingON());
+//     }
+
+//     public void VibrationToggle()
+//     {
+//         dataManager.gameData.setting.VibrationSettingChange(!dataManager.gameData.setting.IsVibrationSettingON());
+//         SetSetting(vibration, dataManager.gameData.setting.IsVibrationSettingON());
+//         vibrationController.VibrationSetting(dataManager.gameData.setting.IsVibrationSettingON());
+//     }
+// }
